7791d8b5ab647c168ceb249c22a44a5fc93d5b8e
lbb <lbb>
2020-01-15 15:10:00 +0800

    修改demo中feign无法启动的bug

--- demo/consumer-feign/src/main/java/com/l/library/demo/consumer/feign/FeignConsumerApplication.java
+++ demo/consumer-feign/src/main/java/com/l/library/demo/consumer/feign/FeignConsumerApplication.java
@@ -4,13 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.cloud.openfeign.FeignClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
 public class FeignConsumerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(FeignClient.class, args);
+        SpringApplication.run(FeignConsumerApplication.class, args);
     }
 }
